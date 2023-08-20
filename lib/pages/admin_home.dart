e0aa05c10663b39e41ffe08b9b6c0c6ff18192ac
Rishabh <rishabhky23@gmail.com>
2023-08-20 12:21:00 +0530

    second commit

--- lib/pages/admin_home.dart
+++ lib/pages/admin_home.dart
@@ -1,6 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:razorpay_flutter/razorpay_flutter.dart';
 import 'package:vmg/utils/routes.dart';
 
 class AdminHome extends StatefulWidget {
@@ -16,11 +20,51 @@ class AdminHome extends StatefulWidget {
 
 class _AdminHomeState extends State<AdminHome> {
   int flatNumber = 0;
+  Razorpay? _razorpay;
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Success : ${response.paymentId}", timeInSecForIosWeb: 4);
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    Fluttertoast.showToast(
+        msg: "Payment Failed : ${response.code} - ${response.message}",
+        timeInSecForIosWeb: 4);
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    Fluttertoast.showToast(
+        msg: "External wallet is : ${response.walletName}",
+        timeInSecForIosWeb: 4);
+  }
+
+  var amountController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
     fetchUserData();
+    _razorpay = Razorpay();
+    _razorpay?.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+    _razorpay?.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+    _razorpay?.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+  }
+
+  void makepayment() async {
+    var options = {
+      'key': 'rzp_test_ugZrbmLHkEGjBy',
+      'amount': (int.parse(amountController.text) * 100)
+          .toString(), //in the smallest currency sub-unit.
+      'name': 'V.M Grandeur',
+      'order_id': 'order_EMBFqjDHEEn80l', // Generate order_id using Orders API
+      'timeout': 300, // in seconds
+      'prefill': {'contact': '', 'email': ''}
+    };
+    try {
+      _razorpay?.open(options);
+    } catch (e) {
+      debugPrint(e.toString());
+    }
   }
 
   Future<void> fetchUserData() async {
@@ -68,32 +112,124 @@ class _AdminHomeState extends State<AdminHome> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Welcome ${widget.username}!",
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Flat No: $flatNumber",
-              style: const TextStyle(
-                color: Colors.white,
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+              Text(
+                "Welcome ${widget.username}!",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w500,
+                ),
               ),
-            ),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Flat No: $flatNumber",
+                style: GoogleFonts.poppins(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      color: Colors.white, width: 2), // Border color and width
+                  borderRadius:
+                      BorderRadius.circular(10), // Adjust the radius as needed
+                ),
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.transparent, // Transparent background color
+                    borderRadius: BorderRadius.circular(
+                        10), // Same radius as the outer container
+                  ),
+                  padding: EdgeInsets.all(
+                      10), // Optional: Add padding inside the inner container
+                  child: Column(
+                    children: [
+                      Text(
+                        "Notice ",
+                        style: GoogleFonts.poppins(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Below are the maintenance details for your flat, the payment button opens a dialog box, enter the right amount and continue with RazorPay safe payment ...",
+                        style: GoogleFonts.poppins(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              ElevatedButton.icon(
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.white,
+                    elevation: 8,
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Payment',
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.poppins(
+                                fontSize: 24,
+                                fontWeight: FontWeight.w500,
+                              )),
+                          content: TextFormField(
+                            controller: amountController,
+                            decoration: const InputDecoration(
+                                hintText: "Enter Maintenance Amount:",
+                                labelText: "Amount "),
+                            keyboardType: TextInputType.number,
+                          ),
+                          actions: [
+                            Center(
+                              child: TextButton(
+                                onPressed: () {
+                                  makepayment(); // Close the dialog
+                                },
+                                child: Text('Pay'),
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  icon: const Icon(Icons.payment),
+                  label: const Text(
+                    "Payment",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.bold),
+                  )),
+              const SizedBox(
+                height: 20,
+              )
+            ],
+          ),
         ),
       ),
       drawer: MyDrawer(username: widget.username),
